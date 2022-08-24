d8123d9eaf40f77a3ce9e4ab543295f15348d9b9
Alex Java <carpalexandrumarian@gmail.com>
2022-08-24 21:20:47 +0300

    Statements

--- src/Statements.java
+++ src/Statements.java
@@ -1,3 +1,14 @@
 public class Statements {
-
+    public static void main(String[] args) {
+        int myVariable=50;
+        myVariable++;
+        myVariable--;
+        System.out.println(myVariable);
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " a test" + " and another" +
+                " test" + " and still more " +
+                "tests");
+        System.out.println("So, a statement is ending with ;");
+    }
 }
