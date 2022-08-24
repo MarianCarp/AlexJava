710ad1fdf9c7cf34f5509084279975eb3c674684
Alex Java <carpalexandrumarian@gmail.com>
2022-08-24 20:41:34 +0300

    I create the class

---
+++ src/MilesToKilometres.java
@@ -0,0 +1,14 @@
+public class MilesToKilometres {
+    public static void main(String[] args) {
+
+    // a mile is equal to 1.609344
+        double kilometres =(100*1.609344);
+        int highScore = 49;
+        ++highScore;
+        if (highScore==50) {
+            System.out.println("This is an expression");
+            System.out.println(kilometres);
+        }
+
+    }
+}
