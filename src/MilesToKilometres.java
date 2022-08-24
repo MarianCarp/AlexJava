049499964e7ce83dcad7f6ade2f3883fe54117e5
Alex Java <carpalexandrumarian@gmail.com>
2022-08-24 21:11:20 +0300

    some expressions

--- src/MilesToKilometres.java
+++ src/MilesToKilometres.java
@@ -2,12 +2,18 @@ public class MilesToKilometres {
     public static void main(String[] args) {
 
     // a mile is equal to 1.609344
-        double kilometres =(100*1.609344);
-        int highScore = 49;
+        double kilometres =(100*1.609344); // the expression is "kilometres =(100*1.609344)"
+        int highScore = 49; // the expression is "highScore = 49"
         ++highScore;
         if (highScore==50) {
-            System.out.println("This is an expression");
+            System.out.println("This is an expression");// the expression is ""This is an expression""
             System.out.println(kilometres);
+
+            int score =100; // the expression is "score =100"
+            if (score>99){ // the expression is "score>99"
+                System.out.println("You got the high score!");// the expression is ""You got the high score!""
+                score = 0;// the expression is "score = 0"
+            }
         }
 
     }
