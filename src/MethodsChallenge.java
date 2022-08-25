54690f6c95436b236bb720241fb72470e4943420
Alex Java <carpalexandrumarian@gmail.com>
2022-08-25 23:38:16 +0300

    another solution

--- src/MethodsChallenge.java
+++ src/MethodsChallenge.java
@@ -2,30 +2,42 @@ public class MethodsChallenge {
     public static void main(String[] args) {
 
         int position = calculateHighScorePosition(1500);
-        displayHighScorePosition("Marko",position);
+        displayHighScorePosition("Marko", position);
         position = calculateHighScorePosition(900);
-        displayHighScorePosition("Vlad",position);
+        displayHighScorePosition("Vlad", position);
         position = calculateHighScorePosition(400);
-        displayHighScorePosition("Lemon",position);
+        displayHighScorePosition("Lemon", position);
         position = calculateHighScorePosition(50);
-        displayHighScorePosition("Alfred",position);
+        displayHighScorePosition("Alfred", position);
+        position = calculateHighScorePosition(1000);
+        displayHighScorePosition("George", position);
     }
 
     public static void displayHighScorePosition(String playerName, int position) {
         System.out.println(playerName + " managed to get into position " + position + " on the high score table!");
     }
-    public static int calculateHighScorePosition(int playerScore){
-        if (playerScore>1000) {
-            return 1;
-        }
-        else if (playerScore>500&&playerScore<=1000){
-            return 2;
-        }
-        else if (playerScore>100&&playerScore<=500){
-            return 3;
-        }
-        else {
-            return 4;
-        }
+
+    public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500) {
+//            return 2;
+//        } else if (playerScore >= 100) {
+//            return 3;
+//        }
+//
+//        return 4;
+
+// another solution:
+        int position = 4;
+        if (playerScore>=1000){
+            position = 1;
+        }else if (playerScore>=500){
+            position = 2;
+        } else if (playerScore >= 100) {
+            position =3;
+        }return position;
+
+
     }
 }
