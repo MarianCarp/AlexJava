2fea41921bcf70acf3664824b8abc37ec2b90a7f
Alex Java <carpalexandrumarian@gmail.com>
2022-08-26 20:09:52 +0300

    PositiveNegativeZero

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,19 @@
+public class PositiveNegativeZero {
+
+    public static void main(String[] args) {
+        checkNumber(5);
+        checkNumber(0);
+        checkNumber(-10);
+    }
+
+    public static void checkNumber(int number){
+        if (number > 0) {
+            System.out.println("Positive number");
+        } else if (number<0) {
+            System.out.println("Negative number");
+        }else {
+            System.out.println("Zero");
+        }
+    }
+
+}
