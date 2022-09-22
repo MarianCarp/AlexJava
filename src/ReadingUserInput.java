cd07a6461ff0c0b92efe575232d5927563cacdf0
Alex Java <42732740+MarianCarp@users.noreply.github.com>
2022-09-22 22:32:39 +0300

    NumberOfDaysInMonth

--- src/ReadingUserInput.java
+++ src/ReadingUserInput.java
@@ -5,17 +5,20 @@ public class ReadingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter your year of birth: ");
         int yearOfBirth = scanner.nextInt();
-        Date d = new Date();
-        int year = d.getYear();
-        int currentYear = year +1900;
-        int age = currentYear-yearOfBirth;
-        System.out.println("Ok, so you have " + age);
         scanner.nextLine(); //handle next line character (enter key)
+
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
-        System.out.println("Your name is " + name);
+
+        int age = 2022-yearOfBirth;
+        if (age>=0&&age<=100){
+            System.out.println("Your name is " + name + " and you are " +age+ " years old.");
+        }else {
+            System.out.println("Invalid year of birth");
+        }
 
         scanner.close();
     }
